a6a5945ef5690dd7177f8e2cd83bef7fba4da5ac
loris.fichera <loris.fichera@9b93cbac-0697-c522-f574-8d8975c4cc90>
2010-05-20 12:02:55 +0000

    miru removed.

--- trunk/sandbox/loris/python-opengl/miru-dev/miru/test/test_docs.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2008 Drew Smathers.
-# See LICENSE for details
-from miru import track, camera, graphics, effects, shader
-__doctests__ = [ track, camera, graphics, effects, shader ]
-
-
