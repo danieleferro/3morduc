cf925ddd829be5d6bbed17037c8826644e815335
loris.fichera <loris.fichera@9b93cbac-0697-c522-f574-8d8975c4cc90>
2010-06-02 14:20:04 +0000

    added corona example

---
+++ sandbox/loris/corona_example.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+#include <corona.h>
+
+using namespace std;
+using namespace corona;
+
+
+int main ()
+{
+  Image * image = 0;
+  image = OpenImage("screenshot_8_103.bmp");
+
+  cout << "Success!" << endl;
+  cout << image->
+  
+
+
+
+
+
+  return 0;
+}
