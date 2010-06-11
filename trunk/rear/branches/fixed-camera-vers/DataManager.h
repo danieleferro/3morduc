71475f175ecf261876b3e2dd37a7451e272f46b3
loris.fichera <loris.fichera@9b93cbac-0697-c522-f574-8d8975c4cc90>
2010-06-11 11:45:08 +0000

    added callback and fixed camera versions under branches.

---
+++ trunk/rear/branches/fixed-camera-vers/DataManager.h
@@ -0,0 +1,57 @@
+#ifndef __OPENGL
+#define __OPENGL
+
+#include <GL/glut.h>    // Header File For The GLUT Library 
+#include <GL/gl.h>	// Header File For The OpenGL32 Library
+#include <GL/glu.h>	// Header File For The GLu32 Library
+
+#endif
+
+#ifndef __STD_LIB_INCLUDE
+#define __STD_LIB_INCLUDE
+
+#include <stdio.h>
+#include <math.h>
+#include <cstdlib> 
+#include <sstream>
+#include <vector>
+#include <iostream>
+
+#endif
+
+#ifndef __DATA_INTERFACE
+#define __DATA_INTERFACE
+
+#include "robot.h"
+#include "texture_png.h"
+#include "DataLogic.h"
+
+
+class DataManager
+{
+ private:
+
+  GLuint _texture[1];
+  Robot * _rob;
+  DataLogic * _logic;
+
+  robot_data * _robot_status;
+  image_data * _bg_image_data;
+
+  /* bind the specified image to a texture */
+  void LoadGLTextures(GLuint * texture, const char* filename);
+
+  /* move camera */
+  void MoveCamera(robot_data delta);
+
+ public:
+  /* data manager conversion constructor */
+  /* first parameter is a robot instance */
+  /* second parameter is the simulation session number 
+     it is only needed for offline testing */
+  DataManager(Robot *, DataLogic *); 
+  ~DataManager();
+  void NextStep();
+};
+
+#endif
