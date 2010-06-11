628675d53e28774d1306ce3aa179937b8bbd87ed
daniele.ferro86 <daniele.ferro86@9b93cbac-0697-c522-f574-8d8975c4cc90>
2010-06-11 14:21:25 +0000

    Robot with white circle visible.

--- trunk/rear/trunk/DataManager.cpp
+++ trunk/rear/trunk/DataManager.cpp
@@ -37,14 +37,13 @@ void DataManager::NextStep() {
   
   _logic->RetrieveData(_robot_status);
 
-  // move robot with _robot_status data
+  /* move robot with _robot_status data
   _rob->Place(_robot_status->x,
 	      _robot_status->y,
 	      _robot_status->theta);
-	      
-  
+  */     
 
-  //_rob->Place(40.f, -70.45f - 30, 0.f);
+  _rob->Place(40.f, -70.45f - 30, 0.f);
 
 
   _logic->SelectImage(_robot_status, _bg_image_data);
