d9b62ef6f4090731c6cca66819442b99ceba3586
daniele.ferro86 <daniele.ferro86@9b93cbac-0697-c522-f574-8d8975c4cc90>
2010-06-16 10:45:13 +0000

    personal algorithm to check if a point is included in a triangle.

--- sandbox/dani/images_point/main.cpp
+++ sandbox/dani/images_point/main.cpp
@@ -12,8 +12,8 @@
 #define Y0 1
 #define M0 2
 
-#define SWEEP_ANGLE 45
-
+#define SWEEP_ANGLE 20
+#define RADIUS      100
 using namespace std;
 
 struct image_data {
@@ -32,103 +32,254 @@ struct robot_data {
 };
 
 
+// A floating point
+struct fPoint
+{
+  float x;
+  float y;
+};
+
+
+float AssignPoints(float distance)
+{
+  /* TODO */
+
+  return distance;
+}
+
+float Sign(fPoint p1, fPoint p2, fPoint p3)
+{
+  return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
+}
+
+bool IsPointInTri(fPoint pt, fPoint v1, fPoint v2, fPoint v3)
+{
+  bool b1, b2, b3;
+
+  b1 = Sign(pt, v1, v2) < 0.0f;
+  b2 = Sign(pt, v2, v3) < 0.0f;
+  b3 = Sign(pt, v3, v1) < 0.0f;
+
+  return ((b1 == b2) && (b2 == b3));
+}
+
+float NormalizeAngle(float angle_deegres) {
+
+
+  if (angle_deegres >= 0 && angle_deegres <= 180)
+    return angle_deegres;
+
+  if (angle_deegres >= -180 && angle_deegres <= 0)
+    return angle_deegres;
+    
+ 
+  return NormalizeAngle(angle_deegres-360);
+
+}
+
+
 float PointAlgorithm(robot_data * robot_status, image_data * bg_image_data) {
 
-  // robot_status->theta is always between [-180; 180]
+  /* translate all system in robot_status->x and 
+     robot_status->y; at the end the system is translated again */
+  float x_zero = robot_status->x;
+  float y_zero = robot_status->y;
 
-  float line_a[3];
-  float line_b[3];
-  float line_c[3];
-  float line_d[3];
+  robot_status->x = 0;
+  robot_status->y = 0;
+  
+  bg_image_data->x = bg_image_data->x - x_zero;
+  bg_image_data->y = bg_image_data->y - y_zero;
+
+
+  // theta must be always between [-180; 180]
+  robot_status->theta = NormalizeAngle(robot_status->theta);
+  bg_image_data->theta = NormalizeAngle(bg_image_data->theta);
+
+
+  float ang_coeff_line_a;
+  float ang_coeff_line_b;
+  float ang_coeff_line_c;
+  float ang_coeff_line_d;
 
   float rhs;
   int sign = 1;
 
-  /* calcolo la retta "a" */
-  line_a[X0] = robot_status->x;
-  line_a[Y0] = robot_status->y;
-  line_a[M0] = tan( TO_RADIANS( (robot_status->theta - 90) ) );
+  /* angular coefficient for line "a" */
+  ang_coeff_line_a = tan( TO_RADIANS(robot_status->theta - 90) );
 
-  /* calcolo la retta "b" */
-  line_b[X0] = robot_status->x;
-  line_b[Y0] = robot_status->y;
-  line_b[M0] = -1 / line_a[M0];
+  /* angular coefficient for line "b" */
+  ang_coeff_line_b = tan( TO_RADIANS(robot_status->theta) );
 
-  /* calcolo la retta "c" */
-  line_c[X0] = robot_status->x;
-  line_c[Y0] = robot_status->y;
-  line_c[M0] = tan( atan( line_b[M0] ) + TO_RADIANS( SWEEP_ANGLE ) );
+  /* angular coefficient for line "c" */
+  ang_coeff_line_c = tan( atan( ang_coeff_line_b ) + TO_RADIANS( SWEEP_ANGLE ) );
 
-  /* calcolo la retta "d" */
-  line_d[X0] = robot_status->x;
-  line_d[Y0] = robot_status->y;
-  line_d[M0] = tan( atan( line_b[M0] ) - TO_RADIANS( SWEEP_ANGLE ) );
+  /* angular coefficient for line "d" */
+  ang_coeff_line_d = tan( atan( ang_coeff_line_b ) - TO_RADIANS( SWEEP_ANGLE ) );
 
-  cout << "Line A: " << line_a[M0] << endl;
-  cout << "Line B: " << line_b[M0] << endl;
-  cout << "Line C: " << line_c[M0] << endl;
-  cout << "Line D: " << line_d[M0] << endl;
-
-  cout << "Point ( "
-       << bg_image_data->x << "; "
-       << bg_image_data->y << "; " 
-       << bg_image_data->theta << " )";
-
-  /* first of all, let's exclude images ahead the robot
-  rhs = line_a[M0] * bg_image_data->x + ( line_a[Y0] - line_a[M0] * line_a[X0] );
-
-  
-  if ( bg_image_data->y >= rhs )
-    {
-
-      cout << " is EXcluded. (control 1)" << endl;
-      
-      return IMAGE_NOT_VALID;
-
-    }
+  /*
+    cout << "Angular coefficient line A: " << ang_coeff_line_a << endl;
+    cout << "Angular coefficient line B: " << ang_coeff_line_b << endl;
+    cout << "Angular coefficient line C: " << ang_coeff_line_c << endl;
+    cout << "Angular coefficient line D: " << ang_coeff_line_d << endl;
   */
 
-  if ( (robot_status->theta > -90 && robot_status->theta < 90  ) ) 
-    sign = 1;
-  else
+  /* find poit A and B to form AOB triangle */
+  // A point (retrieved from line C)
+  float A_x1 = sqrt( pow(RADIUS, 2) / ( pow( ang_coeff_line_c , 2) + 1 ) );
+  float A_y1 = ang_coeff_line_c * A_x1;
+  
+  float A_x2 = -1 * A_x1;
+  float A_y2 = ang_coeff_line_c * A_x2;
+  
+  /*
+    cout << "A_x1: " << A_x1 << "; "
+    << "A_y1: " << A_y1
+    << endl << endl;
+    
+    cout << "A_x2: " << A_x2 << "; "
+    << "A_y2: " << A_y2
+    << endl << endl;
+  */
+    
+
+  /* find poit A and B to form AOB triangle */
+  // B point (retrieved from line D)
+  float B_x1 = sqrt( pow(RADIUS, 2) / ( pow( ang_coeff_line_d , 2) + 1 ) );
+  float B_y1 = ang_coeff_line_d * B_x1;
+
+  float B_x2 = -1 * B_x1;
+  float B_y2 = ang_coeff_line_d * B_x2;
+
+  /*
+    cout << "B_x1: " << B_x1 << "; "
+    << "B_y1: " << B_y1
+    << endl << endl;
+    
+    cout << "B_x2: " << B_x2 << "; "
+    << "B_y2: " << B_y2
+    << endl << endl;
+  */
+
+  if ( robot_status->theta >= 0 && robot_status->theta <= 180 )
     sign = -1;
 
-  /* let's exclude images are outside the plan identified 
-     by the sweep angle: line C
-  
-  rhs = line_c[M0] * bg_image_data->x + ( line_c[Y0] - line_c[M0] * line_c[X0] );
+  if ( robot_status->theta >= -180 && robot_status->theta < 0 )
+    sign = 1;
+   
 
+  // find out which couple values are right (point A)
+  float A_x;
+  float A_y;
 
-  if ( bg_image_data->y*sign <= rhs*sign )
+  rhs = ang_coeff_line_a * A_x1;
+  // cout << sign << " * " << A_y1 << " >= " << rhs << " * " << sign << endl;
+
+  if ( A_y1*sign >= rhs*sign )
     {
 
-      cout << " is EXcluded. (control 2)" << endl;
-      
-      return IMAGE_NOT_VALID;
+      A_x = A_x1;
+      A_y = A_y1;
 
     }
-
-  */
-
-  /* let's exclude images are outside the plan identified 
-     by the sweep angle: line D
-  */
-  
-  rhs = line_d[M0] * bg_image_data->x + ( line_d[Y0] - line_d[M0] * line_d[X0] );
-
-  if ( bg_image_data->y*sign <= rhs*sign )
+  else
     {
 
-      cout << " is EXcluded. (control 2)" << endl;
-      
-      return IMAGE_NOT_VALID;
+      A_x = A_x2;
+      A_y = A_y2;
 
     }
+    
+  cout << "point A chosen: " << A_x << " ; " << A_y << endl;
+
+  // find out which couple values are right (point B)
+  float B_x;
+  float B_y;
+  float B_z = 0;
+
+  rhs = ang_coeff_line_a * B_x1;  
+  // cout << sign << " * " << B_y1 << " >= " << rhs << " * " << sign << endl;
+
+  if ( B_y1*sign >= rhs*sign )
+    {
+
+      B_x = B_x1;
+      B_y = B_y1;
+
+    }
+  else
+    {
+
+      B_x = B_x2;
+      B_y = B_y2;
+
+    }
+    
+  cout << "point B chosen: " << B_x << " ; " << B_y << endl;
+
+  fPoint pt;
+  pt.x = bg_image_data->x;
+  pt.y = bg_image_data->y;
+
+  fPoint v1;
+  v1.x = 0;
+  v1.y = 0;
   
-  */
+  fPoint v2;
+  v2.x = A_x;
+  v2.y = A_y;
   
- 
-  cout << " is INcluded." << endl;
+  fPoint v3;
+  v3.x = B_x;
+  v3.y = B_y;
+  
+  bool flag = IsPointInTri(pt, v1, v2, v3);
+
+
+  /* retraslate system */
+  robot_status->x = x_zero;
+  robot_status->y = y_zero;
+  
+  bg_image_data->x = bg_image_data->x + x_zero;
+  bg_image_data->y = bg_image_data->y + y_zero;
+
+
+  if (flag) 
+    {
+      cout << "Point ( "
+	   << bg_image_data->x << "; "
+	   << bg_image_data->y << "; " 
+	   << bg_image_data->theta << " )";
+      
+      cout << " is included." << endl;
+      
+      float distance;
+      float angle_gap;
+      distance = sqrt (
+		       pow( ( bg_image_data->x - robot_status->x), 2 ) +
+		       pow( ( bg_image_data->y - robot_status->y), 2 ) );
+
+      angle_gap = fabs( bg_image_data->theta - robot_status->theta);
+
+      cout << "Distance from robot values: " << distance << endl;
+      cout << "Angle gap: " << angle_gap << endl;
+      
+
+
+    }
+  else
+    {
+      cout << "Point ( "
+	   << bg_image_data->x << "; "
+	   << bg_image_data->y << "; " 
+	   << bg_image_data->theta << " )";
+      
+      cout << " is NOT included." << endl;
+    }
+
+  cout << "Robot angle: " << robot_status->theta << " deegres." << endl;
+
+
   return 0;
 
 }
@@ -140,17 +291,25 @@ int main(int argc, char** argv) {
   srand(time(NULL));
   int out = rand() % 10 + 1;
 
+  /*
+  if (argc < 2) 
+    {
+      cout << "Usage: ./main <robot_angle_in_deegres>" << endl << endl;
+      exit(0);
+    }
+  */
+
   robot_data robot_status;
   image_data bg_image_data;
 
   robot_status.x = 0;
   robot_status.y = 0;
-  robot_status.theta = -180;
+  robot_status.theta = 0;
 
 
-  bg_image_data.x =  1.5;
-  bg_image_data.y =  1;
-  bg_image_data.theta = 0;
+  bg_image_data.x =  -2;
+  bg_image_data.y =  0.73;
+  bg_image_data.theta = 27;
   
   
   PointAlgorithm(&robot_status, &bg_image_data);
