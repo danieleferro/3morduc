a78c47128133d05eb92b4898cd232c10a4f56671
loris.fichera <loris.fichera@9b93cbac-0697-c522-f574-8d8975c4cc90>
2010-07-22 16:56:22 +0000

    added license

--- rear/trunk/source/texture_png.h
+++ rear/trunk/source/texture_png.h
@@ -1,3 +1,24 @@
+/*
+ * texture_png.h    
+ *
+ * This file is part of REAR.
+ * Copyright (C) 2010 Daniele Ferro (daniele.ferro86@gmail.com) 
+ *                    Loris Fichera (loris.fichera@gmail.com)
+ *
+ * REAR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REAR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with REAR.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __PNG_LOADER_OPENGL__
 #define __PNG_LOADER_OPENGL__
 
