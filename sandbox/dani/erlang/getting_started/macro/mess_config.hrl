43867800b6a9176e921d3a742f352ecc488de8b8
daniele.ferro86 <daniele.ferro86@9b93cbac-0697-c522-f574-8d8975c4cc90>
2011-02-14 16:29:01 +0000

    erlang code & caniglia report deleted from sandbox/dani.

--- sandbox/dani/erlang/getting_started/macro/mess_config.hrl
+++
@@ -1,7 +0,0 @@
-%%%----FILE mess_config.hrl----
-%%% Configure the location of the server node,
--define(server_node, messenger@anatore).
-%%%----END FILE----
-
-%% .hrl files can contain any valid Erlang code but are most
-%% often used for record and macro definitions.
